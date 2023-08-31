2f0f0b369f1279c1b2cbcbd5111e1c2d87acb49b
Jacek Marchwicki <jacek.marchwicki@gmail.com>
2023-08-31 17:56:06 +0200

    feature: allow cycle/lead time calculations

--- queries/tasks.sql
+++ queries/tasks.sql
@@ -6,6 +6,10 @@ SELECT
     type,
     time_created,
     time_started,
-    time_resolved
+    time_resolved,
+    CASE WHEN time_resolved IS NOT NULL AND time_created IS NOT NULL THEN DATE_DIFF(time_resolved, time_created, minute) / 60.0 END as lead_time_hours,
+    CASE WHEN time_resolved IS NOT NULL AND time_started IS NOT NULL THEN DATE_DIFF(time_resolved, time_started, minute) / 60.0 END as cycle_time_hours,
+    CASE WHEN time_resolved IS NOT NULL AND time_created IS NOT NULL THEN DATE_DIFF(time_resolved, time_created, second) END as lead_time_seconds,
+    CASE WHEN time_resolved IS NOT NULL AND time_started IS NOT NULL THEN DATE_DIFF(time_resolved, time_started, second) END as cycle_time_seconds,
 FROM
     `metrics-keys.four_keys.tasks_jira`
